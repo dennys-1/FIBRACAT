36146a8d91470b2a1af3259b65b5b0b2484a7ebf
70474401 <estrella_yani_13@hotmail.com>
2021-05-05 11:01:15 -0500

    .

--- Controllers/AdministradorController.cs
+++ Controllers/AdministradorController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Asp.netCoreMVCCRUD.Models;
 
+
 namespace Asp.netCoreMVCCRUD.Controllers
 {
     public class AdministradorController : Controller
@@ -18,14 +19,14 @@ namespace Asp.netCoreMVCCRUD.Controllers
             _context = context;
         }
 
-        // GET: Employee
+        // GET: Administrador
         public async Task<IActionResult> Index()
         {
             return View(await _context.Admin.ToListAsync());
         }
 
 
-        // GET: Employee/Create
+        // GET: Administrador/Create
         public IActionResult AddOrEdit(int id = 0)
         {
             if (id == 0)
@@ -34,9 +35,6 @@ namespace Asp.netCoreMVCCRUD.Controllers
                 return View(_context.Admin.Find(id));
         }
 
-        // POST: Employee/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddOrEdit([Bind("CID,CORREO,TELEFONO,DNI,DIRECCION")] Administrador administrador)
@@ -54,7 +52,7 @@ namespace Asp.netCoreMVCCRUD.Controllers
         }
 
 
-        // GET: Employee/Delete/5
+        // GET: Administrador/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             var administrador =await _context.Admin.FindAsync(id);
