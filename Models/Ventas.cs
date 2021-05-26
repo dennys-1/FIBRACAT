d0638da1a83d181d5f883dcba6f74e840e77d745
Jhoselyn16 <81030591+Jhoselyn16@users.noreply.github.com>
2021-05-26 01:17:03 -0500

    :::

---
+++ Models/Ventas.cs
@@ -0,0 +1,18 @@
+
+    
+   
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FIBRACAT.Models
+{
+
+
+   public class Ventas
+    {
+     
+    }
+}
