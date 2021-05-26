c6cc0e98804a7faf79b5d7ec6427cea0b6bbe908
Jhoselyn16 <81030591+Jhoselyn16@users.noreply.github.com>
2021-05-26 01:20:01 -0500

    :::

---
+++ Controllers/VentasController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FIBRACAT.Models;
+using FIBRACAT.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace FIBRACAT.Controllers
+{
+    public class VentasController : Controller
+    {
+   
+    }
+}
